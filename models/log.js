21d8247b12bb7ff3cf2b63589493e7d14786fc52
Isaiah Murray <murray.isaiah13@gmail.com>
2021-02-19 12:15:44 -0500

    Created log model

---
+++ models/log.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const Log = db.define("log", {
+  description: {
+    type: DataTypes.STRING,
+  },
+  definition: {
+    type: DataTypes.STRING,
+  },
+  result: {
+    type: DataTypes.STRING,
+  },
+  owner: {
+    type: DataTypes.INTEGER,
+  },
+},
+{});
