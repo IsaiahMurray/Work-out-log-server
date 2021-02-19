9334a9574a73d21b902c3cd0235798e6b7580c07
Isaiah Murray <murray.isaiah13@gmail.com>
2021-02-19 13:17:59 -0500

    updated packages and added db connection

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Sequelize = require("sequelize");
 
-const db = new Sequelize('', 'postgres', 'weewoo123', {
+const db = new Sequelize(`postgres://postgres:${process.env.PASS}@localhost:5432/wol`, {
     host: 'localhost',
     dialect: 'postgres'
 });
